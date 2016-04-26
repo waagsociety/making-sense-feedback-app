651b3f2a5f95218b2b5f3c6954b8c596c8c1de0f
Tim Bormans <hello@timbormans.com>
2016-04-26 09:36:34 +0200

    Use hash history so we can use this on gh-pages

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { IndexRoute, Router, Route, browserHistory } from 'react-router';
+import { IndexRoute, Router, Route, hashHistory } from 'react-router';
 
 import configureStore from './redux/create';
 import App from './containers/App';
@@ -12,7 +12,7 @@ const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={browserHistory}>
+    <Router history={hashHistory}>
       <Route path="/" component={App}>
         <IndexRoute component={Home} />
         <Route path="/sensor/:id" component={Sensor} />
